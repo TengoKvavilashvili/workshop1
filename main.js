316ad8ad8099d4eb31bf1389ab058c4a3ccd259a
TengoKvavilashvili <tengokvavilashvili9@.gmail.com>
2023-04-25 10:56:09 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = "Tengo";
+let lastName = "Kvavilashvili";
+let birthYear = 1990;
+let gender = "male";
+
+
+
+
+let country = "united kingdom";
+let population = 67.33;
+
+console.log(country +"'s population is "+ population + ' ' + 'million');
